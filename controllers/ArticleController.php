1c9170615d98cc2424c92b4f06a51b78f7f49071
Bảo-Ngậu <buithebao83@gmail.com>
2023-03-01 14:46:08 +0700

    update

---
+++ controllers/ArticleController.php
@@ -0,0 +1,16 @@
+<?php
+    class ArticleController{
+        public function index(){
+
+        }
+        public function edit(){
+
+        }
+        public function delete(){
+            
+        }
+        public function add(){
+
+        }
+    }
+?>
