6ad58e9eb60c4bfd9dfc0709154105386f25a7c0
Bảo-Ngậu <buithebao83@gmail.com>
2023-03-04 17:38:28 +0700

    update

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -1,8 +1,19 @@
 <?php 
     class CategoryController{
-        public function index(){}
-        public function add(){}
-        public function edit(){}
-        public function delete(){}
+        public function index(){
+            require_once("./services/CategoryService.php");
+            require_once("./views/category/index_category.php");
+        }
+        public function add(){
+            require_once("./services/CategoryService.php");
+            require_once("./views/category/add_category.php");
+        }
+        public function edit(){
+            require_once("./services/CategoryService.php");
+            require_once("./views/category/edit_category.php");
+        }
+        public function delete(){
+            require_once("./services/CategoryService.php");
+        }
     }
 ?>
--- services/AuthorService.php
+++ services/AuthorService.php
@@ -32,6 +32,77 @@
             }
             return $newArray;
         }
+
+        public function add($ten_tgia,$hinh_tgia){
+            $dbconn = new DBConnection();
+            $conn = $dbconn->getConnection();
+
+            $sql = "INSERT INTO tacgia(ten_tgia,hinh_tgia) VALUES(?,?)";
+            $stmt = $conn->prepare($sql);
+            if($stmt->execute([$ten_tgia,$hinh_tgia])){
+                header("location: ./index.php?controller=author");
+            }else{
+                echo "<script>alert('Thêm thất bại!!')</script>";
+            }
+        }
+        public function edit($ma_tgia,$ten_tgia,$hinh_tgia){
+            $dbconn = new DBConnection();
+            $conn = $dbconn->getConnection();
+
+            $sql = "UPDATE tacgia SET ten_tgia =?,hinh_tgia=? WHERE ma_tgia =?";
+            $stmt = $conn->prepare($sql);
+            if (!$stmt->execute([$ten_tgia,$hinh_tgia,$ma_tgia])){
+                echo "<script>alert('Sửa thất bại')</script>";
+            }else{
+                header('location: index.php?controller=author');
+            }
+        }
+        public function delete($ma_tgia){
+            $dbconn = new DBConnection();
+            $conn = $dbconn->getConnection();
+
+            $sql = "DELETE FROM tacgia WHERE ma_tgia=?";
+            $stmt = $conn->prepare($sql);
+
+            $sql1 = "DELETE FROM baiviet WHERE ma_tgia=?";
+            $stmt1 = $conn->prepare($sql1);
+            $stmt1->execute([$ma_tgia]);
+            if(!$stmt->execute([$ma_tgia])){
+                echo "<script>alert('Thêm thất bại')</script>";
+            }else{
+                header('location: index.php?controller=author');
+            }
+        }
     }
 $author = new AuthorService();
+require_once("AllService.php");
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    if(isset($_POST['ten_tgia']) && isset($_POST['hinh_tgia']) && $_POST['isAction'] == 'add'){
+        $ten_tgia = $_POST['ten_tgia'];
+        $hinh_tgia = $_POST['hinh_tgia'];
+
+        $allService->delForeiginKey('baiviet','baiviet_ibfk_2');
+        $allService->addAutoPrimaryKey('tacgia','ma_tgia');
+        $author->add($ten_tgia,$hinh_tgia);
+        $allService->addForeiginKey('baiviet','ma_tgia','tacgia','ma_tgia','baiviet_ibfk_2');
+        $allService->delAutoPrimaryKey('tacgia','ma_tgia');
+    }elseif(isset($_POST['txtCatId']) && isset($_POST['txtCatName']) && isset($_POST['txtCatImg']) && $_POST['isAction'] == 'edit'){
+        $txtCatId = $_POST['txtCatId'];
+        $txtCatName = $_POST['txtCatName'];
+        $txtCatImg = $_POST['txtCatImg'];
+
+        $author->edit($txtCatId,$txtCatName,$txtCatImg);
+    }
+}
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    if(isset($_GET['id']) && isset($_GET['ten_tgia']) && $_GET['action']=='edit'){
+        $id = $_GET['id'];
+        $ten_tgia = $_GET['ten_tgia'];
+    }
+    elseif(isset($_GET['id']) && $_GET['action']=='delete'){
+        $id = $_GET['id'];
+
+        $author->delete($id);
+    }
+}
 ?>
