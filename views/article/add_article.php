ac98fc9ddc64061d187a672ea47ac0e49a5c9c10
Nguyễn Xuân Ngọc <84265122+baitaptoan53@users.noreply.github.com>
2023-03-04 22:56:29 +0700

    fix edit article page

--- views/article/add_article.php
+++ views/article/add_article.php
@@ -8,7 +8,6 @@
     <title>Thêm bài viết</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="css/style_login.css">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js'></script>
--- views/article/edit_article.php
+++ views/article/edit_article.php
@@ -6,12 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sửa bài viết</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
-        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="css/style_login.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js'></script>
 </head>
 
 <body>
@@ -36,21 +35,9 @@
                         <input type="text" class="form-control" name="ten_bhat" value="<?= $arrayArticle['ten_bhat'] ?>">
                     </div>
                     <div class="input-group mt-3 mb-3">
-                        <span class="input-group-text" id="lblCatId">Mã thể loại</span>
-                        <select class="form-select" aria-label="Default select example" name="ma_tloai">
-                            <?php
-                            foreach($category->getAllCategory("multi") as $value) {
-                                $isSeclected = '';
-                                if ($value['ma_tloai'] == $ma_tloai) {
-                                    $isSeclected = 'selected';
-                                } else {
-                                    $isSeclected = '';
-                                }
-                                echo '
-                                    <option ' . $isSeclected . ' value="'.$value['ma_tloai'].'">' . $value['ma_tloai'] . '-' . $value['ten_tloai'] . '</option>
-                                ';
-                            }
-                            ?>
+                        <span class="input-group-text">Tên thể loại</span>
+                        <select id='ten_tloai'  style="width:250px" name="ten_tloai">
+                            <option value='0'>Chọn tên thể loại</option>
                         </select>
                     </div>
                     <div class="input-group mt-3 mb-3">
@@ -62,22 +49,9 @@
                         <textarea type="text" class="form-control" name="noidung"><?= $arrayArticle['noidung'] ?></textarea>
                     </div>
                     <div class="input-group mt-3 mb-3">
-                        <span class="input-group-text" id="lblCatId">Mã tác giả</span>
-                        <select class="form-select" aria-label="Default select example" name="ma_tgia">
-                            <?php
-                           
-                            foreach($author->getAllAuthor('multi') as $value) {
-                                $isSeclected = '';
-                                if ($value['ma_tgia'] == $ma_tgia) {
-                                    $isSeclected = 'selected';
-                                }else {
-                                    $isSeclected = '';
-                                }
-                                echo '
-                                    <option ' . $isSeclected . ' value="'.$value['ma_tgia'].'">' . $value['ma_tgia'] . '-' . $value['ten_tgia'] . '</option>
-                                ';
-                            }
-                            ?>
+                        <span class="input-group-text">Tên tác giả</span>
+                        <select id='ten_tgia' style="width:250px" name="ten_tgia">
+                            <option value='0'>Chọn tên tác giả</option>
                         </select>
                     </div>
                     <div class="input-group mt-3 mb-3">
@@ -101,5 +75,56 @@
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
         crossorigin="anonymous"></script>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $("#ten_tgia").select2({
+            ajax: {
+                url: "../CSE485_2023_BTTH02/services/get_ten_tgia.php",
+                type: "post",
+                dataType: 'json',
+                delay: 250,
+                data: function(params) {
+                    return {
+                        searchTerm: params.term // search term
+                    };
+                },
+                processResults: function(response) {
+                    return {
+                        results: response
+                    };
+                },
+                cache: true
+            }
+        });
+        $("#ten_tloai").select2({
+            ajax: {
+                url: "../CSE485_2023_BTTH02/services/get_ten_tloai.php",
+                type: "post",
+                dataType: 'json',
+                delay: 250,
+                data: function(params) {
+                    return {
+                        searchTerm: params.term // search term
+                    };
+                },
+                processResults: function(response) {
+                    return {
+                        results: response
+                    };
+                },
+                cache: true
+            }
+        });
+        window.addEventListener("load", function() {
+            var now = new Date();
+            var offset = now.getTimezoneOffset() * 60000;
+            var adjustedDate = new Date(now.getTime() - offset);
+            var formattedDate = adjustedDate.toISOString().substring(0, 16); // For minute precision
+            var datetimeField = document.getElementById("myDatetimeField");
+            datetimeField.value = formattedDate;
+        });
 
+    });
+</script>
 </html>
